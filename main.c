875d8f424371f47e93f6016d4e0720a5ed6d3b18
jos2k4 <joshuapeters439@gmail.com>
2025-08-25 18:28:04 +0200

    basic struktur der ausgabe

--- main.c
+++ main.c
@@ -2,11 +2,35 @@
 #include <stdio.h>
 
 int main(){
-
+while(1) {
   int i;
+  printf("/main \n");
   printf("Wie moechten Sie fortfahren? \n");
-  printf("(1) Alle Aufgaben anzeigen \n(2) Neue Aufgabe erstellen \n(3) Aufgabe nach Namen suchen \n(4) Aufgabe nach Bearbeiter suchen \n");
+  printf("(1) Alle Aufgaben anzeigen \n(2) Neue Aufgabe erstellen \n(3) Offene Aufgaben anzeigen \n(4) Aufgabe nach Bearbeiter suchen \n");
   scanf("%d",&i);
   printf("%d\n",i);
+
+
+  switch(i) {
+    case 1:
+      printf("/main/alle_aufgaben \n");
+      printf("<-- back");
+      scanf("%d",&i);
+    break;
+    case 2:
+      printf("/main/neu_aufgaben \n");
+    break;
+    case 3:
+      printf("/main/offene \n");
+    break;
+    case 4:
+      printf("/main/bearbeiter \n");
+    break;
+
+
+    default:
+      printf("Fehler");
+  }
+}
 
 }
