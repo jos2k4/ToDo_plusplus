f21234fad5025e2cb28a41e97c23104e3a5b8f27
jopet012 <joshua.peters@stud.hn.de>
2025-12-21 19:13:55 +0100

    deleting working (small bug with id, will get fixed soon)

--- main.c
+++ main.c
@@ -6,18 +6,33 @@ int user_input(int i)
 {
   switch(i) {
   case 1:
-    printf("/main/all_assignments \n");
+    printf("/main/all_todos \n");
 
     listTODOS();
 
     break;
   case 2:
-    printf("/main/new_assignment \n");
-    pushTODOS();
+    printf("/main/new_todo \n");
+
+    int temp_id = readTODOS() + 1;
+    char temp_header[100];
+    char temp_description[100];
+    printf("Header: ");
+    scanf("%s", temp_header);
+    printf("Description: ");
+    scanf(" %[^\n]", temp_description);
+
+    pushTODOS(temp_header, temp_description, temp_id);
 
     break;
   case 3:
-    printf("/main/open_assignments \n");
+    printf("/main/delete_todo \n");
+    //listTODOS();
+    int i;
+    printf("Select ID to delete");
+    scanf(" %d", &i);
+    deleteTODOS(i);
+    listTODOS();
     break;
   case 4:
     printf("/main/dev \n");
@@ -32,16 +47,17 @@ int user_input(int i)
   default:
     printf("ERROR");
   }
+
 }
 
 int main(){
-
+readTODOS();
   for (;;)
 {
   int i;
   printf("/main \n");
   printf("Welcome. How do you want to continue? \n");
-  printf("(1) List all Assignments \n(2) Create a new Assignment \n(3) List all open Assignments \n(4) still in development \n(5) exit program \n");
+  printf("(1) List all Assignments \n(2) Create a new Assignment \n(3) Delete a Assignment \n(4) still in development \n(5) exit program \n");
   scanf("%d", &i);
 user_input(i);
     int exit_flag = user_input(i);
--- struct.c
+++ struct.c
@@ -38,26 +38,25 @@ void listTODOS()
 {
     int anzahl = readTODOS();
     readTODOS();
+    printf("*--------------------------------------*\n");
     for (int j = 0; j < anzahl; j++)
     {
         printf("(%d) %s - %s\n", todos[j].id, todos[j].header, todos[j].description);
     }
+    printf("*--------------------------------------*\n");
+
 }
 
-void pushTODOS()
+void pushTODOS(char temp_header[100] , char temp_description[100], int temp_id)
 {
-    char temp_header[100];
-    char temp_description[100];
-    int temp_id = readTODOS() + 1;
-    printf("Header: ");
-    scanf("%s", temp_header);
-    printf("Description: ");
-    scanf(" %[^\n]", temp_description);
 
+    //swap spaces with - so the program can read it
     for (int i = 0; i < strlen(temp_description); i++)
     {
         if (temp_description[i] == ' ')temp_description[i] = '-';
     }
+
+    //reserve memory for descrion
     char *description = malloc(strlen(temp_description) + 1);
     strcpy(description, temp_description);
 
@@ -66,10 +65,53 @@ void pushTODOS()
     if (saves == NULL)perror("Problems opening saves.txt in struct.c/pushTODOS please contact devs");
 
 
-
+    //print array in file
     fprintf(saves, "%s %s %d", temp_header, description, temp_id);
 fclose(saves);
 
 }
 
-void deleteTODOS(){}
+void deleteTODOS(int id_to_delete)
+{
+    int max_nr = readTODOS();
+
+    int found_index = -1;
+
+    // search array for given id
+    for (int i = 0; i < max_nr; i++) {
+        if (todos[i].id == id_to_delete) {
+            found_index = i;
+            break;
+        }
+    }
+
+    // if not found return
+    if (found_index == -1) {
+        printf("ID %d nicht gefunden!\n", id_to_delete);
+        return;
+    }
+
+    //clears the file
+    FILE *delete = fopen("../saves.txt", "w");
+    if (delete == NULL)perror("Problems with clearing saves.txt in struct.c/deleteTODOS please contact devs");
+    fclose(delete);
+
+    //overwrites assignment with given id
+    for (int j = found_index; j < max_nr - 1; j++)
+    {
+        todos[j].header = todos[j+1].header;
+        todos[j].description = todos[j+1].description;
+        todos[j].id = todos[j-1].id;
+
+    }
+
+    //decrease the array by 1
+    max_nr--;
+
+    //print new array in file
+    for (int i = 0; i < max_nr ; i++)pushTODOS(todos[i].header, todos[i].description, todos[i].id);
+
+
+    printf("Todo %d successfully deleted\n", found_index);
+
+}
